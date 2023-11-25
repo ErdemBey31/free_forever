03bd45985ff65db1790fd8fe5a083fcfd0f548b0
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-11-25 10:25:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from threading import Thread
 import os
 
 def run_jupyter():
-  os.system("pip install jupyter && jupyter notebook --ip=0.0.0.0 --port=3000 --allow-root")
+  os.system("pip install jupyter && jupyter notebook --ip=0.0.0.0 --port=3000 --allow-root g")
 
 def run_ssh():
   os.system("ssh -R akls.serveo.net:80:localhost:3000 serveo.net")
