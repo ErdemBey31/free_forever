b097c4bf7fb503c89970042f9a3716c5b292a937
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-11-25 10:18:51 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from threading import Thread
+import os
+
+def run_jupyter():
+  os.system("pip install jupyter && jupyter notebook --ip=0.0.0.0 --port=3000 --allow-root")
+
+def run_ssh():
+  os.system("ssh -R akls.serveo.net:80:localhost:3000 serveo.net")
+
+if True:
+  Thread(target=run_jupyter).start();
+  Thread(target=run_ssh).start();
